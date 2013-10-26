a98d40b3916ea8cb00462f3c8586a302bc38fd23
gurpinars <sami.gurpinar@gmail.com>
2013-10-27 02:07:53 +0300

    stream_response method moved to utils

--- views.py
+++ views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from subprocess import Popen, PIPE, STDOUT
 from django.views.decorators.http import condition
-import re
 from forms import SearchForm
 from django.template import RequestContext
-
+from utils import stream_response
 
 
 def index(request):
@@ -23,20 +21,4 @@ def search(request):
                                             request.POST['search']),mimetype='text/html')
 		
 	
-def stream_response(url, depth, search):
-	cmd = 'python ../python-web-crawler/crawl.py {} {} {}'.format(url, depth, search)
-	p = Popen(cmd, stdout = PIPE, stderr = STDOUT, shell = True)
 
-	yield "<html><body>\n"
-	while True:
-		line = p.stdout.readline()
-
-		m = re.findall(' at (.*)', line)
-		if len(m) > 0:
-			for href in m:
-				yield '<div>Found {} at <a href="{}">{}</a></div>\n'.format(search, href, href)
-		else:
-			yield "<div>%s</div>\n" % line
-			
-		if not line: break
-	yield "</body></html>\n"
